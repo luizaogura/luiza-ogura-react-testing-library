051d0caa79b92cd5d4b496abf9906611eba7463d
Luiza Ogura <luizapogura@gmail.com>
2022-11-22 18:24:03 -0300

    Testa o componente <FavoritePokemon.js />

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './utils/renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+describe('Testa o componente <FavoritePokemon.js />', () => {
+  test('Caso a pessoa não tenha Pokémon favoritos, é exibida na tela a mensagem "No favorite pokemon found"', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemon />);
+
+    const noFavText = getByText(/No favorite pokémon found/i);
+    expect(noFavText).toBeInTheDocument();
+  });
+
+/* test('Testa se são exibidos todos os cards de Pokémon favoritados', () => {
+   const { history } = renderWithRouter(<FavoritePokemon />);
+
+    history.push('/');
+  }); */
+});
