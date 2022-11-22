c0ff0db59bf79d312d271dd9362a922c1780f25e
Luiza Ogura <luizapogura@gmail.com>
2022-11-22 19:01:01 -0300

    Teste se são exibidos todos os cards de Pokémon favoritados

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './utils/renderWithRouter';
 import { FavoritePokemon } from '../pages';
+import App from '../App';
 
 describe('Testa o componente <FavoritePokemon.js />', () => {
   test('Caso a pessoa não tenha Pokémon favoritos, é exibida na tela a mensagem "No favorite pokemon found"', () => {
@@ -10,9 +13,43 @@ describe('Testa o componente <FavoritePokemon.js />', () => {
     expect(noFavText).toBeInTheDocument();
   });
 
-/* test('Testa se são exibidos todos os cards de Pokémon favoritados', () => {
-   const { history } = renderWithRouter(<FavoritePokemon />);
+  test('Testa se são exibidos todos os cards de Pokémon favoritados', () => {
+    const {
+      getByRole,
+      getByText,
+      getAllByText } = renderWithRouter(<App />);
 
-    history.push('/');
-  }); */
+    const homeLink = getByRole('link', { name: 'Home' });
+    userEvent.click(homeLink);
+
+    const eletricCategory = getByRole('button', { name: 'Electric' });
+    const pikachuLink = getByRole('link', { name: 'More details' });
+    userEvent.click(eletricCategory);
+    userEvent.click(pikachuLink);
+
+    const favPikachu = getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(favPikachu);
+    expect(favPikachu).toBeChecked();
+
+    userEvent.click(homeLink);
+
+    const fireCategory = getByRole('button', { name: 'Fire' });
+    const charmanderLink = getByRole('link', { name: 'More details' });
+    userEvent.click(fireCategory);
+    userEvent.click(charmanderLink);
+
+    const favCharmander = getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(favCharmander);
+    expect(favCharmander).toBeChecked();
+
+    const favPokemonLink = getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(favPokemonLink);
+
+    const pikachuFavPage = getByText('Pikachu');
+    const charmanderFavPage = getByText('Charmander');
+    const favorites = getAllByText(/Average weight/i);
+    expect(favorites.length).toBe(2);
+    expect(pikachuFavPage).toBeInTheDocument();
+    expect(charmanderFavPage).toBeInTheDocument();
+  });
 });
